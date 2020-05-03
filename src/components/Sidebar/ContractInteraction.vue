e7d20039815a1e9761c28e0177ab1380e6c089c0
TSEmmaRose <107069005+TSEmmaRose@users.noreply.github.com>
2020-05-03 08:41:21 +0000

    build feeds

--- src/components/Sidebar/ContractInteraction.vue
+++ src/components/Sidebar/ContractInteraction.vue
@@ -1,4 +1,23 @@
 <template>
   <div style="margin-top: 20px;padding: 15px;">
     <el-collapse>
-      <el-collap
+      <el-collapse-item
+        v-for="(contract, index) in parsedContracts"
+        :key="index"
+        :title="contract.title"
+        :name="'deployedContract' + index"
+        :id="'deployedContract' + index"
+      >
+        <el-table :data="contract.abi" :show-header="false" style="width: 100%">
+          <el-table-column>
+            <template slot-scope="scope">
+              <!-- {{ JSON.stringify(scope.row) }} -->
+              <el-row type="flex">
+                <el-col :offset="1" :span="scope.row.loading ? 8 : 7">
+                  <el-button
+                    :id="'deployedContract' + index + scope.row.name"
+                    :loading="scope.row.loading"
+                    style="width:100%"
+                    class="secondaryButton"
+                    type="primary"
+                    @
