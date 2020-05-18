6146cdcdabb1d149e20a6bae002b192485687232
TSEmmaRose <107069005+TSEmmaRose@users.noreply.github.com>
2020-05-18 14:18:49 +0000

    Update meta description

--- src/components/Sidebar/ContractNameSelect.vue
+++ src/components/Sidebar/ContractNameSelect.vue
@@ -1,2 +1,20 @@
 <template>
-  <el-select :id="id+'SelectContract'" v-model="selectedContractModel" class="select" placeholder="C
+  <el-select :id="id+'SelectContract'" v-model="selectedContractModel" class="select" placeholder="Choose a Contract" style="display: block">
+    <el-option v-for="name in contractNames" :key="name" :label="name" :value="name" class="contractNameOption"/>
+  </el-select>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { Mutation, State, Getter } from 'vuex-class'
+const namespace = 'compile'
+@Component
+export default class ContractNameSelect extends Vue {
+
+    @Prop(String) public id!: string
+    @State('selectedContract', { namespace }) public selectedContract!: string
+    @Getter('contractNames', { namespace }) public contractNames!: string[]
+    @Mutation('setSelectedContract', { namespace }) public setSelectedContract!: (contractName: string) => void
+
+    public set selectedContractModel(contractName: string) {
+        this.s
