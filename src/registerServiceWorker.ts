6304ad154254b5f590e9063910f110c6e55c9992
TSEmmaRose <107069005+TSEmmaRose@users.noreply.github.com>
2020-06-04 07:14:07 +0000

    new pdf

---
+++ src/registerServiceWorker.ts
@@ -0,0 +1,20 @@
+/* tslint:disable:no-console */
+
+import { register } from 'register-service-worker'
+
+if (process.env.NODE_ENV === 'production') {
+  register(`${process.env.BASE_URL}service-worker.js`, {
+    ready() {
+      console.log(
+        'App is being served from cache by a service worker.\n' +
+          'For more details, visit https://goo.gl/AFskqB'
+      )
+    },
+    cached() {
+      // console.log('Content has been cached for offline use.')
+    },
+    updated() {
+      console.log('New content is available; please refresh.')
+      window.location.reload()
+    },
+    offlin
