0864fb45b6dd72523939ec9aa524c9a19aff1b06
TSEmmaRose <107069005+TSEmmaRose@users.noreply.github.com>
2020-06-26 15:39:12 +0000

    test stuff

---
+++ src/types/index.d.ts
@@ -0,0 +1,8 @@
+
+declare module 'vue-codemirror'
+declare module 'vue-draggable-resizable'
+declare module 'solhint/lib/index'
+declare module 'web3-utils'
+declare module 'aion-web3'
+declare module 'aion-browser-solc'
+declare module 'vue-analytics'
