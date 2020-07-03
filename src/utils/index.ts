b6d4b0275c906046da4c7479a711b834fc21d611
TSEmmaRose <107069005+TSEmmaRose@users.noreply.github.com>
2020-07-03 21:42:07 +0000

    Update armcom2.py

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,35 @@
-import { MethodAbi } fro
+import { MethodAbi } from 'ethereum-types'
+
+export const parseDeployedContract = (
+  contractName: string,
+  contractAddress: string,
+  abi: MethodAbi[]
+) => {
+  return {
+    contractAddress,
+    title: `${contractName} at ${shortenAddress(contractAddress)}`,
+    abi: abi
+      .filter(i => i.type !== 'constructor' && i.type !== 'event')
+      .map(piece => {
+        return {
+          ...piece,
+          combinedInputs: combineInputs(piece.inputs),
+          loading: false,
+          argsModel: '',
+          res: undefined
+        }
+      })
+  }
+}
+
+export const combineInputs = (
+  inputs: Array<{ name: string; type: string }>
+) => {
+  return inputs
+    .map(({ name, type }) => {
+      return `${type.trim()} ${name.trim()}`
+    })
+    .join(',')
+}
+
+export const extractConst
