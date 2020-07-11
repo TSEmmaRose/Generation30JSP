30d98e688e144d8a6b907096b21e84e49339c031
TSEmmaRose <107069005+TSEmmaRose@users.noreply.github.com>
2020-07-11 22:30:43 +0000

    update defconfig

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -32,4 +32,18 @@ export const combineInputs = (
     .join(',')
 }
 
-export const extractConst
+export const extractConstructor = (abiDefinition: MethodAbi[]) => {
+  for (const item of abiDefinition) {
+    if (item.type === 'constructor') {
+      return combineInputs(item.inputs)
+    }
+  }
+  return undefined
+}
+
+export const shortenAddress = (address: string): string => {
+  const len = address.length
+  return `${address.slice(0, 5)}...${address.slice(len - 5, len)}`
+}
+
+export * from './constants'
